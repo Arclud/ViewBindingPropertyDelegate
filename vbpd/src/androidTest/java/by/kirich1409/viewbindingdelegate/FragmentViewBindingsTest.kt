061fbc86fa9b5de669e13494b40e8591e86399de
Kirill Rozov <krl.rozov@gmail.com>
2020-09-10 18:10:51 +0300

    Setup Fragment tests

---
+++ vbpd/src/androidTest/java/by/kirich1409/viewbindingdelegate/FragmentViewBindingsTest.kt
@@ -0,0 +1,15 @@
+package by.kirich1409.viewbindingdelegate
+
+import androidx.fragment.app.Fragment
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FragmentViewBindingsTest {
+
+    @Test
+    fun name() {
+        TODO("Not yet implemented")
+    }
+}
